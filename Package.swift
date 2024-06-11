d0af0f18eb76f5f7a21d642446a874180a8c591c
Illia Chemolosov <ilya.chemolosov@quickblox.com>
2024-06-11 19:19:39 +0200

    3.0.0
    - Add Quickblox dependencies

--- Package.swift
+++ Package.swift
@@ -13,12 +13,15 @@ let package = Package(
             targets: ["QuickbloxWebRTCWrapper"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/QuickBlox/ios-google-webrtc", .upToNextMajor(from: "124.0.0"))
+        .package(url: "https://github.com/QuickBlox/ios-google-webrtc", .upToNextMajor(from: "124.0.0")),
+        .package(url: "https://github.com/QuickBlox/ios-quickblox-sdk", .upToNextMajor(from: "2.19.0")),
     ],
     targets: [
         .target(
             name: "QuickbloxWebRTCWrapper",
-            dependencies: ["QuickbloxWebRTC", .product(name: "WebRTC", package: "ios-google-webrtc")],
+            dependencies: ["QuickbloxWebRTC", 
+                .product(name: "WebRTC", package: "ios-google-webrtc"),
+                .product(name: "Quickblox", package: "ios-quickblox-sdk")],
             path: "QuickbloxWebRTC-Wrapper"
         ),
         .binaryTarget(
